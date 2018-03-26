7cd7a99887f17532e112734c6875e327741269a4
Yalamandarao <yalamandarao.i@gmail.com>
2018-03-26 16:43:52 +0800

    Created sample example

---
+++ SplashScreen/SplashScreen/SplashScreenPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SplashScreen
+{
+    public partial class SplashScreenPage : ContentPage
+    {
+        public SplashScreenPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
